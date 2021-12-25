1bd4446997c270c9ca6f61497133a5ff862e860d
Thomas Fox <fischer@seitenbau.com>
2021-12-25 11:55:30 +0100

    add tests for equals() and hashCode() in Glyph

---
+++ src/test/java/com/github/thomasfox/gfxfont/GlyphTest.java
@@ -0,0 +1,114 @@
+package com.github.thomasfox.gfxfont;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GlyphTest
+{
+  private Glyph glyph1;
+
+  private Glyph glyph2;
+
+  @BeforeEach
+  void fillGlyphs()
+  {
+    glyph1 = new Glyph();
+    glyph1.bitmap = new boolean[][] {{true, false}, {false, true}};
+    glyph1.bitmapOffset = 0;
+    glyph1.width = 2;
+    glyph1.height = 3;
+    glyph1.xAdvance = 3;
+    glyph1.xOffset = 4;
+    glyph1.yOffset = -10;
+
+    glyph2 = new Glyph();
+    glyph2.bitmap = new boolean[][] {{true, false}, {false, true}};
+    glyph2.bitmapOffset = 0;
+    glyph2.width = 2;
+    glyph2.height = 3;
+    glyph2.xAdvance = 3;
+    glyph2.xOffset = 4;
+    glyph2.yOffset = -10;
+  }
+
+  @Test
+  void testEquals_same()
+  {
+    assertThat(glyph1.equals(glyph1)).isTrue();
+  }
+
+  @Test
+  void testEquals_null()
+  {
+    assertThat(glyph1.equals(null)).isFalse();
+  }
+
+  @Test
+  void testOtherClass_null()
+  {
+    assertThat(glyph1.equals("")).isFalse();
+  }
+
+  @Test
+  void testEquals_equal()
+  {
+    assertThat(glyph1.equals(glyph2)).isTrue();
+  }
+
+  @Test
+  void testEquals_bitmapOffsetNotEqual()
+  {
+    glyph2.bitmapOffset = 99;
+    assertThat(glyph1.equals(glyph2)).isFalse();
+  }
+
+  @Test
+  void testEquals_widthNotEqual()
+  {
+    glyph2.width = 99;
+    assertThat(glyph1.equals(glyph2)).isFalse();
+  }
+
+  @Test
+  void testEquals_heightNotEqual()
+  {
+    glyph2.height = 99;
+    assertThat(glyph1.equals(glyph2)).isFalse();
+  }
+
+  @Test
+  void testEquals_xAdvanceNotEqual()
+  {
+    glyph2.xAdvance = 99;
+    assertThat(glyph1.equals(glyph2)).isFalse();
+  }
+
+  @Test
+  void testEquals_xOffsetNotEqual()
+  {
+    glyph2.xOffset = 99;
+    assertThat(glyph1.equals(glyph2)).isFalse();
+  }
+
+  @Test
+  void testEquals_yOffsetNotEqual()
+  {
+    glyph2.yOffset = 99;
+    assertThat(glyph1.equals(glyph2)).isFalse();
+  }
+
+  @Test
+  void testEquals_bitmapNotEqual()
+  {
+    glyph2.bitmap = new boolean[][] {{false, false}, {false, false}};
+    assertThat(glyph1.equals(glyph2)).isFalse();
+  }
+
+  @Test
+  void testHashCode_equal()
+  {
+    assertThat(glyph1.hashCode()).isEqualTo(glyph2.hashCode());
+  }
+}
